215a7f78eb9fa325d4045db251221b1efc89c542
JPisaBrony <jdpohl789@gmail.com>
2013-11-17 19:22:27 -0600

    re-added BlockLightBlock

--- common/lightProcessing/block/BlockLightBlock.java
+++ common/lightProcessing/block/BlockLightBlock.java
@@ -1,8 +1,5 @@
 package LightProcessing.common.lightProcessing.block;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Random;
-import java.util.Set;
 
 
 import LightProcessing.common.lib.Methods.*;
@@ -24,22 +21,11 @@ import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.DamageSource;
-import net.minecraft.world.ChunkPosition;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockLightBlock extends Block{
-	
-	///variables
-	private boolean xpos;
-	private boolean xneg;
-	private boolean zpos;
-	private boolean zneg;
-	private double speed;
-	private double cSpeed;
-	private Set blocksNeedingUpdate = new HashSet();
-	private boolean wiresProvidePower = true;
-	
+
 	public BlockLightBlock(int par1, Material par2Material) {
 		super(par1, par2Material);
 		this.setLightValue(1.0F);
@@ -73,15 +59,16 @@ public class BlockLightBlock extends Block{
 	public boolean metaTest(World world, int x, int y, int z, int test){
 		if(Methods.xPosMeta(world,x,y,z) == test && Methods.xPosID(world,x,y,z) == IDRef.LIGHT_BLOCK_ID){
 			return true;
-		if (par1IBlockAccess.getBlockId(par2, par3, par4) == IDRef.LIGHT_BLOCK_ID)
+		}
 		if(Methods.xNegMeta(world,x,y,z) == test && Methods.xNegID(world,x,y,z) == IDRef.LIGHT_BLOCK_ID){
 			return true;
-		if (!par1IBlockAccess.doesBlockHaveSolidTopSurface(par2, par3, par4))
+		}
 		if(Methods.zPosMeta(world,x,y,z) == test && Methods.xPosID(world,x,y,z) == IDRef.LIGHT_BLOCK_ID){
 			return true;
-		if (par5 == 0 && par1IBlockAccess.getBlockId(par2, par3, par4) != IDRef.LIGHT_BLOCK_ID)
+		}
 		if(Methods.zNegMeta(world,x,y,z) == test && Methods.xNegID(world,x,y,z) == IDRef.LIGHT_BLOCK_ID){
 			return true;
+		}
 		return false;
 	}
 	@SideOnly(Side.CLIENT)
@@ -89,7 +76,7 @@ public class BlockLightBlock extends Block{
     {
 		if(par1World.getBlockId(par2, par3 - 1, par4) == 3){
 			par1World.setBlock(par2, par3 - 1, par4, 2);
-		if (!par1World.isRemote)
+		}
 		par1World.setBlockMetadataWithNotify(par2, par3, par4, 0, 3);
         return par9;
     }
@@ -138,6 +125,7 @@ public class BlockLightBlock extends Block{
 		}
 		if(par5Entity instanceof EntityPlayer){
 			((EntityPlayer)par5Entity).addPotionEffect(new PotionEffect(Potion.nightVision.getId(),400,50));
+		}
 		
 		
 		//cures zombie villagers
@@ -147,9 +135,6 @@ public class BlockLightBlock extends Block{
 	        entityvillager.copyLocationAndAnglesFrom(par5Entity);
 	        entityvillager.func_110161_a((EntityLivingData)null);
 	        entityvillager.func_82187_q();
-			par5Entity.worldObj.removeEntity(par5Entity);
-			entityvillager.addPotionEffect(new PotionEffect(Potion.confusion.id, 200, 0));
-			par5Entity.worldObj.playAuxSFXAtEntity((EntityPlayer) null, 1017, (int) par5Entity.posX, (int) par5Entity.posY, (int) par5Entity.posZ, 0);
 
 		        if (((EntityZombie) par5Entity).isChild())
 		        {
@@ -168,6 +153,7 @@ public class BlockLightBlock extends Block{
 		
 		if(par5Entity.isCreatureType(EnumCreatureType.monster, true)){
 		par5Entity.setFire(5);
+		}
 		
 		//Light Motion (Horizontal)
 		
@@ -175,20 +161,23 @@ public class BlockLightBlock extends Block{
 			
 			if(par5Entity.motionX > 1.0 && par5Entity instanceof net.minecraft.entity.item.EntityItem){
 				par5Entity.motionX = 1.0;
-			else if (par5Entity.motionX < -1.0 && par5Entity instanceof net.minecraft.entity.item.EntityItem)
+			}
 			if(par5Entity.motionX < -1.0 && par5Entity instanceof net.minecraft.entity.item.EntityItem){
 				par5Entity.motionX = -1.0;
-			else if (par5Entity.motionZ > 1.0 && par5Entity instanceof net.minecraft.entity.item.EntityItem)
+			}
 			if(par5Entity.motionZ > 1.0 && par5Entity instanceof net.minecraft.entity.item.EntityItem){
 				par5Entity.motionZ = 1.0;
-			else if (par5Entity.motionZ < -1.0 && par5Entity instanceof net.minecraft.entity.item.EntityItem)
+			}
 			if(par5Entity.motionZ < -1.0 && par5Entity instanceof net.minecraft.entity.item.EntityItem){
 				par5Entity.motionZ = -1.0;
+			}
 			
 			boolean xpos = (par1World.doesBlockHaveSolidTopSurface(par2 + 1, par3, par4));
 			boolean xneg = (par1World.doesBlockHaveSolidTopSurface(par2 - 1, par3, par4));
 			boolean zpos = (par1World.doesBlockHaveSolidTopSurface(par2, par3, par4 + 1));
 			boolean zneg = (par1World.doesBlockHaveSolidTopSurface(par2, par3, par4 - 1));
+			double speed = 0.2;
+			double cSpeed = 0.25;
 			
 			if(xpos && zpos && zneg && !xneg){
 				par5Entity.motionX = -speed;
@@ -198,15 +187,16 @@ public class BlockLightBlock extends Block{
 			}
 			else if(zpos && xpos && xneg && !zneg){
 				par5Entity.motionZ = -speed;
-			else if (zneg && xpos && xneg && !zpos)
+			}
 			
 			else if(zneg && xpos && xneg && !zpos){
 				par5Entity.motionZ = speed;
+			}
 			
 			else if(xpos && zpos){
 				if(par5Entity instanceof net.minecraft.entity.item.EntityItem){
-				if (par5Entity instanceof net.minecraft.entity.item.EntityItem)
 					par5Entity.motionY = 0;
+				}
 				
 				if(Math.abs(par5Entity.motionX) > Math.abs(par5Entity.motionZ) && par5Entity.motionX > 0){
 					par5Entity.motionX = cSpeed;
@@ -216,12 +206,12 @@ public class BlockLightBlock extends Block{
 					par5Entity.motionX = -speed;
 					par5Entity.motionZ = cSpeed;
 				}
-			} 
 				
 			}
 			else if(xneg && zneg){
 				if(par5Entity instanceof net.minecraft.entity.item.EntityItem){
 					par5Entity.motionY = 0;
+				}
 				if(Math.abs(par5Entity.motionX) > Math.abs(par5Entity.motionZ) && par5Entity.motionX < 0){
 					par5Entity.motionX = -cSpeed;
 					par5Entity.motionZ = speed;
@@ -233,10 +223,9 @@ public class BlockLightBlock extends Block{
 			}
 			else if(xpos && zneg){
 				if(par5Entity instanceof net.minecraft.entity.item.EntityItem){
-			}
-				if(Math.abs(par5Entity.motionX) > Math.abs(par5Entity.motionZ) && par5Entity.motionX > 0){
 					par5Entity.motionY = 0;
-				else if (Math.abs(par5Entity.motionX) > Math.abs(par5Entity.motionZ) && par5Entity.motionX > 0) {
+				}
+				if(Math.abs(par5Entity.motionX) > Math.abs(par5Entity.motionZ) && par5Entity.motionX > 0){
 					par5Entity.motionX = cSpeed;
 					par5Entity.motionZ = speed;
 				}
@@ -247,10 +236,9 @@ public class BlockLightBlock extends Block{
 			}
 			else if(xneg && zpos){
 				if(par5Entity instanceof net.minecraft.entity.item.EntityItem){
-			}
-				if(Math.abs(par5Entity.motionX) > Math.abs(par5Entity.motionZ) && par5Entity.motionX < 0){
 					par5Entity.motionY = 0;
-				else if (Math.abs(par5Entity.motionX) > Math.abs(par5Entity.motionZ) && par5Entity.motionX < 0) {
+				}
+				if(Math.abs(par5Entity.motionX) > Math.abs(par5Entity.motionZ) && par5Entity.motionX < 0){
 					par5Entity.motionX = -cSpeed;
 					par5Entity.motionZ = -speed;
 				}
@@ -271,79 +259,10 @@ public class BlockLightBlock extends Block{
 			}
 			else if(Math.abs(par5Entity.motionX) < Math.abs(par5Entity.motionZ) && par5Entity.motionZ < 0){
 				par5Entity.motionZ = par5Entity.motionZ - speed;
+			}
 		}
 		
 	}
-	
-	
-	/*public void onNeighborBlockChange(World World, int x, int y, int z, int neighborID) {
-		
-		if (neighborID == IDRef.LIGHT_BLOCK_ID && Methods.isPowered(World, x, y, z)) {
-			World.setBlock(x, y, z, 0, 0, 2);
-		}
-		
-		
-	}*/
-	
-	//copy
-    private void updateAndPropagateCurrentStrength(World par1World, int par2, int par3, int par4)
-    {
-        this.calculateCurrentChanges(par1World, par2, par3, par4, par2, par3, par4);
-        ArrayList arraylist = new ArrayList(this.blocksNeedingUpdate);
-        this.blocksNeedingUpdate.clear();
-
-        for (int l = 0; l < arraylist.size(); ++l)
-        {
-            ChunkPosition chunkposition = (ChunkPosition)arraylist.get(l);
-            par1World.notifyBlocksOfNeighborChange(chunkposition.x, chunkposition.y, chunkposition.z, this.blockID);
-        }
-    }
-
-    private void calculateCurrentChanges(World par1World, int par2, int par3, int par4, int par5, int par6, int par7)
-    {
-        int k1 = par1World.getBlockMetadata(par2, par3, par4);
-        byte b0 = 0;
-        int l1 = this.getMaxCurrentStrength(par1World, par5, par6, par7, b0);
-        this.wiresProvidePower = false;
-        int i2 = par1World.getStrongestIndirectPower(par2, par3, par4);
-        this.wiresProvidePower = true;
-
-        if (i2 > 0 && i2 > l1 - 1)
-        {
-            l1 = i2;
-        }
-
-        int j2 = 0;
-
-        for (int k2 = 0; k2 < 4; ++k2)
-        {
-            int l2 = par2;
-            int i3 = par4;
-
-            if (k2 == 0)
-            {
-                l2 = par2 - 1;
-            }
-
-            if (k2 == 1)
-            {
-                ++l2;
-            }
-
-            if (k2 == 2)
-            {
-                i3 = par4 - 1;
-            }
-
-            if (k2 == 3)
-            {
-                ++i3;
-            }
-		
-            if (l2 != par5 || i3 != par7)
-            {
-                j2 = this.getMaxCurrentStrength(par1World, l2, par3, i3, j2);
-            }
 	@SideOnly(Side.CLIENT)
 	public void spread(World world, int x, int y, int z){
 		 int thisMeta = world.getBlockMetadata(x, y, z);
@@ -383,105 +302,6 @@ public class BlockLightBlock extends Block{
 	
 	public boolean isOpaqueCube()
 	{
-        {
-            par1World.setBlockMetadataWithNotify(par2, par3, par4, l1, 2);
-            this.blocksNeedingUpdate.add(new ChunkPosition(par2, par3, par4));
-            this.blocksNeedingUpdate.add(new ChunkPosition(par2 - 1, par3, par4));
-            this.blocksNeedingUpdate.add(new ChunkPosition(par2 + 1, par3, par4));
-            this.blocksNeedingUpdate.add(new ChunkPosition(par2, par3 - 1, par4));
-            this.blocksNeedingUpdate.add(new ChunkPosition(par2, par3 + 1, par4));
-            this.blocksNeedingUpdate.add(new ChunkPosition(par2, par3, par4 - 1));
-            this.blocksNeedingUpdate.add(new ChunkPosition(par2, par3, par4 + 1));
-        }
-    }
-    
-    private int getMaxCurrentStrength(World par1World, int par2, int par3, int par4, int par5)
-    {
-        if (par1World.getBlockId(par2, par3, par4) != this.blockID)
-        {
-            return par5;
-        }
-        else
-        {
-            int i1 = par1World.getBlockMetadata(par2, par3, par4);
-            return i1 > par5 ? i1 : par5;
-        }
-    }
-	
-	 ////////start
-	/*
-	 public void onNeighborBlockChange(World par1World, int par2, int par3, int par4, int par5) {
-		 
-		 if(par1World.getBlockMetadata(par2, par3, par4) == 2 || par1World.getBlockMetadata(par2, par3, par4) == 3){
-				if(par1World.getBlockId(par2 + 1, par3, par4) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2 + 1, par3, par4) ==0){
-					par1World.setBlockMetadataWithNotify(par2 + 1, par3, par4, 3, 3);
-				}
-				if(par1World.getBlockId(par2 - 1, par3, par4) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2 - 1, par3, par4) ==0){
-					par1World.setBlockMetadataWithNotify(par2 - 1, par3, par4, 3, 3);
-				}
-				if(par1World.getBlockId(par2, par3, par4 + 1) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2, par3, par4 + 1) ==0){
-					par1World.setBlockMetadataWithNotify(par2, par3, par4 + 1, 3, 3);
-				}
-				if(par1World.getBlockId(par2, par3, par4 - 1) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2, par3, par4 - 1) ==0){
-					par1World.setBlockMetadataWithNotify(par2, par3, par4 - 1, 3, 3);
-				}
-		 }
-		 
-		 if(Methods.isPowered(par1World, par2, par3, par4) && par1World.getBlockMetadata(par2, par3, par4) == 0){
-				par1World.setBlockMetadataWithNotify(par2, par3, par4, 1, 3);
-				if(par1World.getBlockId(par2 + 1, par3, par4) == IDRef.LIGHT_BLOCK_ID){
-				par1World.setBlockMetadataWithNotify(par2 + 1, par3, par4, 2, 3);
-				}
-				if(par1World.getBlockId(par2 - 1, par3, par4) == IDRef.LIGHT_BLOCK_ID){
-				par1World.setBlockMetadataWithNotify(par2 - 1, par3, par4, 2, 3);
-				}
-				if(par1World.getBlockId(par2, par3, par4 + 1) == IDRef.LIGHT_BLOCK_ID){
-				par1World.setBlockMetadataWithNotify(par2, par3, par4 + 1, 2, 3);
-				}
-				if(par1World.getBlockId(par2, par3, par4 - 1) == IDRef.LIGHT_BLOCK_ID){
-				par1World.setBlockMetadataWithNotify(par2, par3, par4 - 1, 2, 3);
-				}
-			}
-			if(!Methods.isPowered(par1World, par2, par3, par4) && par1World.getBlockMetadata(par2, par3, par4) == 1)
-			{
-				par1World.setBlockMetadataWithNotify(par2, par3, par4, 0, 3);
-			}
-			if(par1World.getBlockMetadata(par2, par3, par4) == 2){
-				if(par1World.getBlockId(par2 + 1, par3, par4) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2 + 1, par3, par4) != 1){
-					if(par1World.getBlockId(par2 - 1, par3, par4) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2 - 1, par3, par4) != 1){
-					if(par1World.getBlockId(par2, par3, par4 + 1) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2, par3, par4 + 1) != 1){
-					if(par1World.getBlockId(par2, par3, par4 - 1) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2, par3, par4 - 1) != 1){
-						if(par1World.getBlockId(par2 + 1, par3, par4) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2 + 1, par3, par4) == 3){
-							par1World.setBlockMetadataWithNotify(par2 + 1, par3, par4, 2, 3);
-						}
-						if(par1World.getBlockId(par2 - 1, par3, par4) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2 - 1, par3, par4) == 3){
-							par1World.setBlockMetadataWithNotify(par2 - 1, par3, par4, 2, 3);
-						}
-						if(par1World.getBlockId(par2, par3, par4 + 1) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2, par3, par4 + 1) == 3){
-							par1World.setBlockMetadataWithNotify(par2, par3, par4 + 1, 2, 3);
-						}
-						if(par1World.getBlockId(par2, par3, par4 - 1) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2, par3, par4 - 1) == 3){
-							par1World.setBlockMetadataWithNotify(par2, par3, par4 - 1, 2, 3);
-						}
-						if(par1World.getBlockId(par2 + 1, par3, par4) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2 + 1, par3, par4) == 0){
-							if(par1World.getBlockId(par2 - 1, par3, par4) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2 - 1, par3, par4) == 0){
-							if(par1World.getBlockId(par2, par3, par4 + 1) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2, par3, par4 + 1) == 0){
-							if(par1World.getBlockId(par2, par3, par4 - 1) == IDRef.LIGHT_BLOCK_ID && par1World.getBlockMetadata(par2, par3, par4 - 1) == 0){
-						par1World.setBlockMetadataWithNotify(par2, par3, par4, 0, 3);
-							}
-							}
-							}
-						}
-					}		
-				}
-			}
-		}
-		}
-			
-	 }
-*/
-    //////// end
-	 
 	    return false;
 	}
 	
