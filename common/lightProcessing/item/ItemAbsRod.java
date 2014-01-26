b9e26bdd2855fcf8162d018a060d670721a92ee0
Meoco55 <rellik.enigma@gmail.com>
2014-01-26 00:54:13 -0500

    updating
    getting back on track

--- common/lightProcessing/item/ItemAbsRod.java
+++ common/lightProcessing/item/ItemAbsRod.java
@@ -5,9 +5,13 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatMessageComponent;
+import net.minecraft.world.World;
 
 public class ItemAbsRod extends Item{
 
@@ -15,6 +19,8 @@ public class ItemAbsRod extends Item{
 		super(par1);
 		this.setUnlocalizedName("AbsRod");
 		this.setCreativeTab(ItemTab.itemTab);
+		this.setMaxDamage(50);
+		this.maxStackSize = 1;
 		}
 		@Override
 		public boolean hasEffect(ItemStack par1ItemStack){
@@ -30,4 +36,22 @@ public class ItemAbsRod extends Item{
 		    {
 			  itemIcon = iconRegister.registerIcon(Methods.textureName(this.getUnlocalizedName()));
 		}
+		  public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
+		    {
+			  if(!par3World.isRemote){
+				  int essence = ExtractionList.getEssence(par3World, par4, par5, par6);
+				  if(essence > 0){
+					  par2EntityPlayer.addChatMessage("Worth " + Integer.toString(essence) + " light essence.");
+				  }
+				  else if(essence < 0)
+					  par2EntityPlayer.addChatMessage("Worth " + Integer.toString(essence * -1) + " dark essence.");
+				  else{
+					  par2EntityPlayer.addChatMessage("Worth 0 essence.");
+				  }
+				  }
+			  par1ItemStack.useItemRightClick(par3World, par2EntityPlayer);
+			  par1ItemStack.damageItem(1, par2EntityPlayer);
+			  return true;
+			  
+		    }
 		}
