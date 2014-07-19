bc001037f8c32589e866f2b44f7cec318b2bb12e
JPisaBrony <jdpohl789@gmail.com>
2014-07-18 20:29:40 -0500

    updated version and added mod info file

--- common/lib/Reference.java
+++ common/lib/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
 
 	public static final String MOD_ID = "lightprocessing";
 	public static final String NAME = "Light Processing";
-	public static final String VERSION = "0.1";
+	public static final String VERSION = "0.11";
 	
 }
